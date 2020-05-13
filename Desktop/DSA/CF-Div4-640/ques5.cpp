bb06ba5a5a2aac951ba56b80fc13e8690c5694e5
2018A7PS0147G <f20180147@goa.bits-pilani.ac.in>
2020-05-13 22:31:30 +0530

    'cfround641div4'

---
+++ Desktop/DSA/CF-Div4-640/ques5.cpp
@@ -0,0 +1,191 @@
+//rohitaas_15
+#include<bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+//typedef tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> o_set;
+#define ll  long long
+//#define int ll
+#define um(a,b) unordered_map<a,b>
+#define fastio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define all(v) v.begin(),v.end()
+#define f(a,b) for(int ii=a;ii<=b;ii++)
+#define pb push_back
+#define mp make_pair
+#define pii pair<ll,ll>
+#define ff first
+#define ss second
+#define lb(v,x) lower_bound(v.begin(),v.end(),x)
+#define ub(v,x) upper_bound(v.begin(),v.end(),x)
+#define show(x) cout<<x<<"\n"
+#define printvector(v,n) f(0,n-1){cout<<v[i]<<" ";}
+#define input(v,n) f(0,n-1){cin>>v[i];}
+#define vr vector<ll>
+#define check() cout<<"hi"<<"\n"
+#define MOD 1000000007
+//#define endl "\n"
+#define len() length()
+#define maxv(v) *max_element(all(v));
+#define minv(v) *min_element(all(v));
+#define sumv(v) accumulate(all(v),0)
+#define test() ll t;cin>>t;while(t--)
+ll search(vr arr, ll l, ll r, ll x){if (r >= l) {ll mid = l + (r - l) / 2;if (arr[mid] == x)return mid;if (arr[mid] > x)return search(arr, l, mid - 1, x);return search(arr, mid + 1, r, x);} return -1;}
+ll gcd(ll a, ll b){if(!b)return a;return gcd(b, a % b);}
+ll lcm(ll a , ll b){return (a*b)/ gcd(a,b);}
+ll power(ll x, ll y, ll p){ll res = 1;x = x % p;while (y > 0){ if (y & 1) res = (res*x) % p; y = y>>1;x = (x*x) % p; } return res; }
+ll modInverse(ll n, ll p){return power(n, p-2, p);}
+ll CmP(ll n, ll r, ll p=MOD){if (r==0)return 1;ll fac[n+1];fac[0] = 1;for (ll i=1 ; i<=n; i++)fac[i] = fac[i-1]*i%p; return (fac[n]* modInverse(fac[r], p) % p *modInverse(fac[n-r], p) % p) % p; }
+/*ll constructSTUtil(vr v,ll start,ll end,int *st,ll index)
+{
+	if(start==end)
+	{
+		st[index]=v[start];
+		return st[index];
+	}
+	ll mid=(start+end)/2;
+	st[index]=constructSTUtil(v,start,mid,st,2*index)+constructSTUtil(v,mid+1,end,st,2*index+1);
+	return st[index];
+}
+ll *constructST(vr v,ll n)
+{
+	ll po=(ll)(ceil(log2(n)));
+	ll msize=2*power(2,po)+1;
+	vr st[msize];
+	constructSTUtil(v,0,n-1,st,1);
+	return st;
+}*/
+ll ask1(ll y)
+{
+	cout<<1<<" "<<y<<endl;
+	ll x;
+	cin>>x;
+	return x;
+}
+bool compare(pair<ll,ll> &a,pair<ll,ll> &b)
+{
+	return a.second<b.second;
+}
+signed main()
+{
+	fastio();///*
+	/*#ifndef ONLINE_JUDGE
+	 freopen("input.txt","r",stdin);
+	 freopen("output.txt","w",stdout);
+	 #endif
+	 */
+
+	 int t;
+	 cin>>t;
+	 int cccc=t;
+	while(t--)
+	 {
+	 	int n;
+	 	cin>>n;
+	 	vector<int> in(n+1);
+	 	f(1,n)
+	 	{
+	 		cin>>in[ii];
+	 	}
+	 	ll c=0;
+	 	f(1,n)
+	 	{
+	 		ll i=1;
+	 		ll j=1;
+	 		ll sum=0;
+	 		ll flag=0;
+	 		ll gflag=0;
+	 		while(i!=n+1)
+	 		{
+	 		    //show(sum<<" "<<ii);
+	 			if(gflag)
+	 			{
+	 				if(sum==in[ii] && i!=j)
+	 				{
+	 					flag=1;
+	 					break;
+	 				}
+	 				else if(sum==in[ii] && i==j)
+                    {
+                        gflag=0;
+                        i++;
+                        j++;
+                        sum=0;
+                    }
+	 				else
+	 				{
+	 					if(sum>in[ii])
+	 					{
+	 						if(i==j)
+	 						{
+	 							gflag=0;
+	 							i++;
+	 							j++;
+	 							sum=0;
+	 						}
+	 						else
+	 						{
+	 							sum-=in[j];
+	 							j++;
+	 						}
+	 					}
+	 					else
+	 					{
+	 						gflag=0;
+	 						i++;
+	 					}
+	 				}
+
+	 			}
+	 			else
+	 			{
+	 				sum+=in[i];
+		 			if(sum==in[ii] && i!=j)
+		 			{
+		 				flag=1;
+		 				break;
+		 			}
+		 			else if(sum==in[ii] && i==j)
+                    {
+                        gflag=0;
+                        i++;
+                        j++;
+                        sum=0;
+                    }
+		 			else if(sum>in[ii])
+		 			{
+		 				gflag=1;
+		 				if(i==j)
+		 				{
+		 					gflag=0;
+		 					i++;
+		 					j++;
+		 					sum=0;
+
+		 				}
+		 				else
+		 				{
+
+		 					sum-=in[j];
+		 					j++;
+		 				}
+		 			}
+		 			else
+		 			{
+		 				i++;
+		 			}
+	 			}
+
+	 		}
+	 		if(flag)
+	 		{
+	 			c++;
+	 		}
+	 	}
+	 	show(c);
+	 }
+
+
+	 	/*cout<<"Case #"<<cccc-t<<": ";*/
+
+}
---
+++ Desktop/DSA/CF-Div4-640/ques7.cpp
@@ -0,0 +1,149 @@
+//rohitaas_15
+#include<bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+//typedef tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> o_set;
+#define ll  long long
+#define int ll
+#define um(a,b) unordered_map<a,b>
+#define fastio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define all(v) v.begin(),v.end()
+#define f(a,b) for(ll i=a;i<=b;i++)
+#define pb push_back
+#define mp make_pair
+#define pii pair<ll,ll>
+#define ff first
+#define ss second
+#define lb(v,x) lower_bound(v.begin(),v.end(),x)
+#define ub(v,x) upper_bound(v.begin(),v.end(),x)
+#define show(x) cout<<x<<"\n"
+#define printvector(v,n) f(0,n-1){cout<<v[i]<<" ";}
+#define input(v,n) f(0,n-1){cin>>v[i];}
+#define vr vector<ll>
+#define check() cout<<"hi"<<"\n"
+#define MOD 1000000007
+//#define endl "\n"
+#define len() length()
+#define maxv(v) *max_element(all(v));
+#define minv(v) *min_element(all(v));
+#define sumv(v) accumulate(all(v),0)
+#define test() ll t;cin>>t;while(t--)
+ll search(vr arr, ll l, ll r, ll x){if (r >= l) {ll mid = l + (r - l) / 2;if (arr[mid] == x)return mid;if (arr[mid] > x)return search(arr, l, mid - 1, x);return search(arr, mid + 1, r, x);} return -1;}
+ll gcd(ll a, ll b){if(!b)return a;return gcd(b, a % b);}
+ll lcm(ll a , ll b){return (a*b)/ gcd(a,b);}
+ll power(ll x, ll y, ll p){ll res = 1;x = x % p;while (y > 0){ if (y & 1) res = (res*x) % p; y = y>>1;x = (x*x) % p; } return res; }
+ll modInverse(ll n, ll p){return power(n, p-2, p);}
+ll CmP(ll n, ll r, ll p=MOD){if (r==0)return 1;ll fac[n+1];fac[0] = 1;for (ll i=1 ; i<=n; i++)fac[i] = fac[i-1]*i%p; return (fac[n]* modInverse(fac[r], p) % p *modInverse(fac[n-r], p) % p) % p; }
+/*ll constructSTUtil(vr v,ll start,ll end,int *st,ll index)
+{
+	if(start==end)
+	{
+		st[index]=v[start];
+		return st[index];
+	}
+	ll mid=(start+end)/2;
+	st[index]=constructSTUtil(v,start,mid,st,2*index)+constructSTUtil(v,mid+1,end,st,2*index+1);
+	return st[index];
+}
+ll *constructST(vr v,ll n)
+{
+	ll po=(ll)(ceil(log2(n)));
+	ll msize=2*power(2,po)+1;
+	vr st[msize];
+	constructSTUtil(v,0,n-1,st,1);
+	return st;
+}*/
+ll ask1(ll y)
+{
+	cout<<1<<" "<<y<<endl;
+	ll x;
+	cin>>x;
+	return x;
+}
+bool compare(pair<ll,ll> &a,pair<ll,ll> &b)
+{
+	return a.second<b.second;
+}
+signed main()
+{
+	fastio();///*
+	/*#ifndef ONLINE_JUDGE
+	 freopen("input.txt","r",stdin);
+	 freopen("output.txt","w",stdout);
+	 #endif
+	 */
+
+	 ll t;
+	 cin>>t;
+	 ll cccc=t;
+	while(t--)
+	 {
+	 	ll n;
+	 	cin>>n;
+	 	deque<ll> q;
+	 	q.pb(1);
+	 	vr visited(n+1,0);
+	 	visited[1]=1;
+	 	ll mflag=0;
+	 	f(1,n-1)
+	 	{
+	 	    if(q.size()==n)
+            {
+                break;
+            }
+	 		ll flag=0;
+	 		for(ll j=1;j<=n;j++)
+	 		{
+	 		    //show(j);
+
+	 			if(visited[j]!=1)
+	 			{
+	 				ll temp1=q.front();
+	 				ll temp2=q.back();
+	 				if(abs(j-temp1)<=4 && abs(j-temp1)>=2)
+	 				{
+	 					flag=1;
+	 					q.push_front(j);
+	 					visited[j]=1;
+	 					break;
+	 				}
+	 				if(abs(j-temp2)<=4 && abs(j-temp2)>=2)
+	 				{
+	 					flag=1;
+	 					q.pb(j);
+	 					visited[j]=1;
+	 					break;
+	 				}
+
+	 			}
+	 		}
+	 		////show(q.front());
+	 		//show(q.back());
+	 		if(!flag)
+	 		{
+	 			mflag=1;
+	 			show(-1);
+	 			break;
+	 		}
+	 		else
+	 		{
+	 			continue;
+	 		}
+	 	}
+	 	if(!mflag)
+	 	{
+	 		while(!q.empty())
+	 		{
+	 			cout<<q.front()<<" ";
+	 			q.pop_front();
+	 		}
+	 		show("");
+	 	}
+	 }
+
+
+	 	/*cout<<"Case #"<<cccc-t<<": ";*/
+
+}
